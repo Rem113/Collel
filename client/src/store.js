54bb35707af915d92e21985dc0ebd1bcc9df3b5e
Hillel Saal <remi.saal@gmail.com>
2019-02-27 14:05:45 +0200

    Deleted redux console from store

--- client/src/store.js
+++ client/src/store.js
@@ -1,12 +1,13 @@
-import { createStore, applyMiddleware, compose } from 'redux'
-import rootReducer from './reducers'
-import thunk from 'redux-thunk'
+import { createStore, applyMiddleware } from "redux"
+import rootReducer from "./reducers"
+import thunk from "redux-thunk"
 
 const middlewares = [thunk]
 
 const initialState = {}
 
-export default createStore(rootReducer, initialState, compose(
-    applyMiddleware(...middlewares),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-))
+export default createStore(
+	rootReducer,
+	initialState,
+	applyMiddleware(...middlewares)
+)
