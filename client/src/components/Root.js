3cd6e83249252f4918b74ad342ca3c78681fa9bf
Hillel Saal <remi.saal@gmail.com>
2019-03-02 23:39:45 +0200

    La cam avec les boutons

--- client/src/components/Root.js
+++ client/src/components/Root.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types"
 import { Provider } from "react-redux"
 import { BrowserRouter as Router, Route } from "react-router-dom"
 import Home from "./pages/Home"
-import CoursePage from "./pages/CoursePage"
+import Course from "./pages/Course"
 import AddCoursePage from "./pages/AddCoursePage"
 import DeleteCoursePage from "./pages/DeleteCoursePage"
 import EditCoursePage from "./pages/EditCoursePage"
@@ -16,15 +16,15 @@ const Root = ({ store }) => (
 	<MuiThemeProvider theme={theme}>
 		<Provider store={store}>
 			<Router>
-				<div className="h-100">
+				<React.Fragment>
 					<Route exact path="/" component={Home} />
 					<Route exact path="/course/add" component={AddCoursePage} />
-					<Route exact path="/course/id/:id" component={CoursePage} />
+					<Route exact path="/course/id/:id" component={Course} />
 					<Route exact path="/tag/:tag/:filter?" component={CourseList} />
 					<Route exact path="/course/list" component={DeleteCoursePage} />
 					<Route exact path="/course/edit/:id" component={EditCoursePage} />
 					<HomeFab />
-				</div>
+				</React.Fragment>
 			</Router>
 		</Provider>
 	</MuiThemeProvider>
