2e041958b55b2ec8f69b46731f183cf967da001f
Hillel Saal <remi.saal@gmail.com>
2019-03-11 21:40:37 +0200

    Changed card and list image resolution

--- client/src/components/controls/CourseCard.js
+++ client/src/components/controls/CourseCard.js
@@ -50,7 +50,7 @@ class CourseCard extends Component {
 						component="img"
 						alt={course.title}
 						className={classes.media}
-						image={`http://img.youtube.com/vi/${course.link}/mqdefault.jpg`}
+						image={`http://img.youtube.com/vi/${course.link}/hqdefault.jpg`}
 						title={course.title}
 					/>
 					<CardContent>
